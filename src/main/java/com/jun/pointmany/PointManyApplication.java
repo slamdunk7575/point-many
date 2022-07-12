d636c07bf5aaed01670f594b525290c1aeddbf25
slamdunk7575 <slamdunk7575@gmail.com>
2022-07-12 22:00:17 +0900

    (feat) properties 설정 추가

---
+++ src/main/java/com/jun/pointmany/PointManyApplication.java
@@ -0,0 +1,13 @@
+package com.jun.pointmany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointManyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PointManyApplication.class, args);
+    }
+
+}
